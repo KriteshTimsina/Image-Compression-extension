ca27902302e7b4734f9ae57bc28126e45e9e86c4
Kritesh Timsina <kriteshtimsina55@gmail.com>
2023-01-09 22:37:57 +0545

    add menu slide only when image selected functionality

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const upload = document.querySelector(".upload");
 const inputField = document.querySelector("#input");
-const showImage = document.getElementById("image-field")
+const showImage = document.querySelector(".image");
+const hiddenMenus = document.getElementsByClassName("forms")[0];
+
 const handleInputClick = () => {
     const file = inputField.files[0];
-
     showImage.src = URL.createObjectURL(file);
-    showImage.style.width = "140px";
-
+    showImage.style.width = "100%";
+    showImage.className = ".image.active";
+    inputField.disabled = true;
+    hiddenMenus.className = ".forms.active"
 }
 
 inputField.addEventListener('change', handleInputClick)
+
+upload.addEventListener('click', () => inputField.click())
 
